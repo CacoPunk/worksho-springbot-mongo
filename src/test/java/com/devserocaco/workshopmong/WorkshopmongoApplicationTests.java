914f6dac93fc35d473a58dddc50944b4f41f588d
Paulo Ricardo Silva dos Santos Filho <desenvolvimento4@codi.com.br>
2023-06-21 12:24:07 -0300

    First commit

---
+++ src/test/java/com/devserocaco/workshopmong/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devserocaco.workshopmong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
